9cdf9d0e23dcba26f6c719aa7a10ac561c5ebcea
LuanTreuke <128757273+LuanTreuke@users.noreply.github.com>
2024-11-29 22:40:12 -0300

    autowired

--- AtividadePraticaEscola/src/main/java/br/edu/ifpr/atividadepraticaescola/AtividadePraticaEscolaApplication.java
+++ AtividadePraticaEscola/src/main/java/br/edu/ifpr/atividadepraticaescola/AtividadePraticaEscolaApplication.java
@@ -1,12 +1,37 @@
 package br.edu.ifpr.atividadepraticaescola;
 
+import br.edu.ifpr.atividadepraticaescola.entidades.Responsavel;
+import br.edu.ifpr.atividadepraticaescola.repositorios.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
 public class AtividadePraticaEscolaApplication {
+
+	@Autowired
+	private AlunoRep alunoRep;
+
+	@Autowired
+	private ContatoRep contatoRep;
+
+	@Autowired
+	private CursoRep cursoRep;
+
+	@Autowired
+	private DisciplinaRep disciplinaRep;
+
+	@Autowired
+	private ProfessorRep professorRep;
+
+	@Autowired
+	private ResponsavelRep responsavelRep;
+
+	@Autowired
+	private TurmaRep turmaRep;
+
+	@Autowired
+	private UsuarioRep usuarioRep;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtividadePraticaEscolaApplication.class, args);
